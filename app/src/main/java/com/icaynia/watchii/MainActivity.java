1fb031ed24b965276049a1a9ad75ff74f404ae0b
Sion Yu <icaynia@icaynia.com>
2016-07-31 17:35:21 +0900

    First commit
    처음 커밋

---
+++ app/src/main/java/com/icaynia/watchii/MainActivity.java
@@ -0,0 +1,35 @@
+package com.icaynia.watchii;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextClock;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SeekBar seekbar = (SeekBar) findViewById(R.id.seekBar);
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
+                TextClock textClock = (TextClock) findViewById(R.id.textClock);
+                textClock.setTextSize(progress*2 + 50);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+    }
+}
